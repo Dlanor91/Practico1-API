bc6e080ca45af79216c97da42ce43cc1a274bde0
Ronald Lima Nunez <limaronald926@gmail.com>
2022-02-06 02:16:56 -0300

    ejercicio 1 listo sin validar

--- js/codigoEj1.js
+++ js/codigoEj1.js
@@ -1,27 +1,64 @@
-mostrarPaises()
+//OCulto el div de mostrar los datos
+document.querySelector("#mostrarDatos").style.display="none";
 
-function mostrarPaises(){
-   fetch("https://restcountries.com/v3.1/all")         //Primero la URL de la API 
-
-   .then(function(response){                            //llamo mi primer response de JSON como un return 
-      return  response.json();
-    }
-    )
-
-    .then(function(data)                                //capturo los datos con los que trabajare
-    {
-        //console.log(data);                            //reviso que me devuelva los valores
-        for (let i = 0; i < data.length; i++) {
-            const paises = data[i];
-            let option = document.createElement("option");
-            option.text = paises.name.common;
-            option.value = paises.name.common;
-            document.querySelector("#country").appendChild(option);
-        };        
+function mostrarPaises(ciudad) {
+  fetch("https://restcountries.com/v3.1/all") //Primero la URL de la API
+    .then(function (response) {
+      //llamo mi primer response de JSON como un return
+      return response.json();
     })
-    .catch(function(error){                             //dejo mi catch para capturar errores
-        console.error(error);
-    }
-    )
-    
+
+    .then(function (data) //capturo los datos con los que trabajare
+    {
+      console.log(data);                            //reviso que me devuelva los valores
+      for (let i = 0; i < data.length; i++) {
+        const paises = data[i];
+        if(paises.cca2===ciudad){
+            document.querySelector("#country").innerHTML = paises.name.nativeName.spa.common;
+            break;
+        }
+      }
+    })
+    .catch(function (error) {
+      //dejo mi catch para capturar errores
+      console.error(error);
+    });
+}
+
+document.querySelector("#enviarDatos").addEventListener("click", mostrarDatosClima);
+
+function mostrarDatosClima() {     
+    let city= document.querySelector("#city").value;     
+    document.querySelector("#mostrarDatos").style.display="block";
+    let url=`https://api.openweathermap.org/data/2.5/weather?appid=a2a4e23e0043c30a6c155907672aaf71&q=${city}&units=metric&lang=es`;
+  fetch(url)
+    .then(function (response) {
+      //llamo mi primer response de JSON como un return
+      return response.json();
+    })
+
+    .then(function (data)                             //capturo los datos con los que trabajare
+    {
+      console.log(data); //reviso que me devuelva los valores     
+      if(data.cod >=200 && data.cod <300) {
+        mostrarPaises(data.sys.country);
+        document.querySelector("#tempActual").innerHTML = data.main.temp;
+        document.querySelector("#tempMin").innerHTML = data.main.temp_min;
+        document.querySelector("#tempMax").innerHTML = data.main.temp_max;
+        document.querySelector("#description").innerHTML = data.weather[0].description;
+        let idIcon = data.weather[0].icon;
+        let srcIcon = `http://openweathermap.org/img/wn/${idIcon}@2x.png`;
+        document.querySelector("#icon").src = srcIcon;       
+        }else{            
+                document.querySelector("#country").innerHTML = "La ciudad no existe";  
+                document.querySelector("#tempActual").innerHTML = "-";
+                document.querySelector("#tempMin").innerHTML = "-";
+                document.querySelector("#tempMax").innerHTML = "-";                
+                document.querySelector("#icon").src = "";       
+        }
+    })
+    .catch(function (error) {
+      //dejo mi catch para capturar errores
+      console.error(error);
+    });
 }
